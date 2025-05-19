883a05a79e913a86efb451abed7940ed07ba5183
Hiwo-2022 <dilacher78@gmail.com>
2025-05-19 18:26:35 -0400

    single product component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./Components/Header/Header";
 import Carousel from "./Components/Carousel/Carousel";
 import Category from "./Components/Category/Category";
+import Product from "./Components/Product/Product";
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Carousel />
       <Category />
+      <Product />
     </>
   );
 }
