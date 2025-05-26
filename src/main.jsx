1fe2c61ca42cc2d73f854aabcc595f38e6bf6bea
Hiwo-2022 <dilacher78@gmail.com>
2025-05-26 15:14:35 -0400

    main jsx updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { reducer,initialState } from './Utility/reducer.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom"; // Add this import
+import { reducer, initialState } from "./Utility/reducer.jsx";
 
-import App from './App.jsx'
-import { DataProvider } from './Components/DataProvider/DataProvider.jsx'
+import App from "./App.jsx";
+import { DataProvider } from "./Components/DataProvider/DataProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <DataProvider reducer={reducer} initialState={initialState}>
-      <App />
-    </DataProvider>
+    <BrowserRouter basename="/Amazon-Clone">
+      <DataProvider reducer={reducer} initialState={initialState}>
+        <App />
+      </DataProvider>
+    </BrowserRouter>
   </StrictMode>
 );
