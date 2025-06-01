d0e41bad9e6adb36bc074c1740543be8b0f3115e
Hiwo-2022 <dilacher78@gmail.com>
2025-05-31 22:59:03 -0400

    payment page ui built

--- src/Pages/Payment/Payment.jsx
+++ src/Pages/Payment/Payment.jsx
@@ -1,12 +1,95 @@
-import React from 'react'
-import LayOut from '../../Components/LayOut/LayOut'
+import React, { useContext, useState } from "react";
+import LayOut from "../../Components/LayOut/LayOut";
+import classes from "./Payment.module.css";
+import { DataContext } from "../../Components/DataProvider/DataProvider";
+import ProductCard from "../../Components/Product/ProductCard";
+import { useStripe, useElements, CardElement } from "@stripe/react-stripe-js";
+import CurrencyFormat from "../../Components/CurrancyFormat/CurrancyFormat";
 
 function Payment() {
+  const [{ user, basket }] = useContext(DataContext);
+
+  const totalItem = basket?.reduce((amount, item) => {
+    return item.amount + amount;
+  }, 0);
+
+  const total = basket.reduce((amount, item) => {
+    return item.price * item.amount + amount;
+  }, 0);
+
+  const [cardError, setCardError] = useState(null);
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const handlechange = (e) => {
+    e?.error?.message ? setCardError(e?.error?.message) : setCardError(null);
+  };
+
+  const handlePayment =(e)=> {
+    e.preventDefault()
+  }
+
   return (
     <LayOut>
-      <h1>paye here</h1>
+      {/* header */}
+      <div className={classes.payment__header}>
+        Checkout ({totalItem}) items
+      </div>
+      {/* payment method */}
+      <section className={classes.payment}>
+        {/* address */}
+        <div className={classes.flex}>
+          <h3>Delivery Adress</h3>
+          <div>
+            <div>{user?.email}</div>
+            <div>1234 fullstack ave</div>
+            <div>Seattle,WA</div>
+          </div>
+        </div>
+        <hr />
+
+        {/* product */}
+        <div className={classes.flex}>
+          <h3>Review Items and Delivery</h3>
+          <div>
+            {basket?.map((item) => (
+              <ProductCard product={item} flex={true} />
+            ))}
+          </div>
+        </div>
+        <hr />
+
+        {/* card form */}
+        <div className={classes.flex}>
+          <h3>Payment Methods</h3>
+          <div className={classes.payment__card}>
+            <div className={classes.payment__detail}>
+              <form onSubmit={handlePayment}>
+                {/* card error */}
+                {cardError && (
+                  <small style={{ color: "red" }}>{cardError}</small>
+                )}
+                {/* card element */}
+                <CardElement onChange={handlechange} />
+                {/* price */}
+                <div className={classes.payment__price}>
+                  <div>
+                    <span style={{ display: "flex", gap: "10px" }}>
+                      <p>Total Order |</p>{" "}
+                      <span style={{ marginTop: "16px" }}>
+                        <CurrencyFormat amount={total} />
+                      </span>
+                    </span>
+                  </div>
+                  <button>Pay Now</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
     </LayOut>
-  )
+  );
 }
 
-export default Payment
+export default Payment;
--- src/Router.jsx
+++ src/Router.jsx
@@ -7,20 +7,30 @@ import Orders from "./Pages/Orders/Orders";
 import Results from "./Pages/Results/Results";
 import Cart from "./Pages/Cart/Cart";
 import ProductDetail from "./Pages/ProductDetail/ProductDetail";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+const stripePromise = loadStripe(
+  "pk_test_51RTyNGJUfFjoXvpiWWVcfY19Sm8pxrYlJ9wXtr0rlr0tyjkYVAqWj8L2Fy6YdHsQ5Wfw12pI1aNgC4pOZqtzQmRw00BCKYJ9BY"
+);
 
 function Routing() {
   return (
-    
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/auth" element={<Auth />} />
-        <Route path="/payments" element={<Payment />} />
-        <Route path="/orders" element={<Orders />} />
-        <Route path="/category/:categoryName" element={<Results />} />
-        <Route path="/products/:productId" element={<ProductDetail />} />
-        <Route path="/cart" element={<Cart />} />
-      </Routes>
-    
+    <Routes>
+      <Route path="/" element={<Landing />} />
+      <Route path="/auth" element={<Auth />} />
+      <Route
+        path="/payments"
+        element={
+          <Elements stripe={stripePromise}>
+            <Payment />
+          </Elements>
+        }
+      />
+      <Route path="/orders" element={<Orders />} />
+      <Route path="/category/:categoryName" element={<Results />} />
+      <Route path="/products/:productId" element={<ProductDetail />} />
+      <Route path="/cart" element={<Cart />} />
+    </Routes>
   );
 }
 
