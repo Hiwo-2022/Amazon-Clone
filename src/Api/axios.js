46ebea29f4e5bfcfa9fccccbc84b0852c580bdb7
Hiwo-2022 <dilacher78@gmail.com>
2025-06-02 19:54:45 -0400

    deployment in render

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,9 +1,11 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  //baseURL: "http://127.0.0.1:5001/clone-e82a7/us-central1/api", // local development
-  
+  // baseURL: "http://127.0.0.1:5001/clone-e82a7/us-central1/api", // local development
+
   baseURL: "https://us-central1-clone-e82a7.cloudfunctions.net/api", // deployed on Firebase
+
+  // baseURL: "https://amazon-api-deploy-kg4u.onrender.com/", // deployed on Render
 });
 
 
